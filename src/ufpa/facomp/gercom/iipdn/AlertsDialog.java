878dac23d9ce2aa5bf562b258fde28361fa522d5
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-02-23 23:13:50 -0300

    Adicionado comentários

--- src/ufpa/facomp/gercom/iipdn/AlertsDialog.java
+++ src/ufpa/facomp/gercom/iipdn/AlertsDialog.java
@@ -7,24 +7,28 @@ public class AlertsDialog extends Alert {
 	
 	private String headerText;
 
+	
+	//Construtor para caixas de diálgos simples
 	public AlertsDialog(AlertType alertType) {
 		super(alertType);
 		
 		this.show();
 	}
 
+	
+	//Contrutor para caixas de diálogos com conteúdo em texto editável 
 	public AlertsDialog(AlertType alertType, String contentText, ButtonType... buttons) {
 		super(alertType, contentText, buttons);
 		
 		this.show();
 	}
 	
+	
+	//Contrutor para caixas de diálogos com texto editável na header e no conteúdo 
 	public AlertsDialog(AlertType alertType,String headerText,String contentText,ButtonType... buttons) {
 		super(alertType, contentText, buttons);
 		this.headerText=headerText;
 		
 		this.show();
 	}
-	
-
 }
