77eb8ff5467d25f6c5bc4de511400d7cdd635610
MarlonWSantos <marlon.santos.santos@icen.ufpa.br>
2020-02-25 15:40:41 -0300

    Adicionado alerts no try...catch, verifyIsObserving e alert se observando

--- src/ufpa/facomp/gercom/iipdn/Main.java
+++ src/ufpa/facomp/gercom/iipdn/Main.java
@@ -1,18 +1,23 @@
 package ufpa.facomp.gercom.iipdn;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 
 public class Main extends Application {
 
-		
+
 	private static Stage screenLoading = null;
 
 	@Override
@@ -24,9 +29,18 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("IIPDN - GUI CoAP Client");
 			primaryStage.show();
-			
+
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+				@Override
+				public void handle(WindowEvent we) {
+
+					verifyIsObserving(we);					
+				}
+			});
+
 		} catch(Exception e) {
-			e.printStackTrace();
+
+			new AlertsDialog(e);
 		}
 	}
 
@@ -34,24 +48,51 @@ public class Main extends Application {
 		launch(args);
 
 	}
-	
+
 	//Cria o PopUp loading na tela do usuário
 	public void showScreenLoading() {
 		Pane loading = null;
+
 		try {
 			loading = (Pane)FXMLLoader.load(getClass().getResource("alert.fxml"));
+			Scene loadingScene = new Scene(loading);
+			screenLoading = new Stage();
+			screenLoading.setScene(loadingScene);
+			screenLoading.show();
+
 		} catch (IOException e) {
-			e.printStackTrace();
+			new AlertsDialog(e);
 		}
-		Scene loadingScene = new Scene(loading);
-		screenLoading = new Stage();
-		screenLoading.setScene(loadingScene);
-		screenLoading.show();		
+
 	}
-	
+
 	//Fecha o PopUp Loading da tela do usuário
 	public void closeScreenLoading() {
 		screenLoading.close();
+	}
+
+	//Verifica se não está ocorrendo observação
+	public void verifyIsObserving(WindowEvent we) {
+
+		Controller ctrl = new Controller();
+
+		//Se houver uma observação em andamento aviso usuário
+		if(ctrl.isObserving()) {
+
+			AlertsDialog alertInterrupt = new AlertsDialog(AlertType.CONFIRMATION);
+			alertInterrupt.setContentText("There is an observation in progress\nReally want to close the application?");
+
+			Optional<ButtonType> result = alertInterrupt.showAndWait();
+
+			//Se usuário escolher sim, para o(s) thread(s) e finaliza aplicação
+			if(result.get()== ButtonType.OK) {
+				new ThreadsObserve();
+
+				//do contrário, aborta finalização da aplicação
+			}else {
+				we.consume();
+			}												
+		} 
 	}
 
 }
